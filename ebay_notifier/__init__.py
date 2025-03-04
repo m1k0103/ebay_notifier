50252774fe097524184de43b92c30478dbb8d9d0
mikok <84585119+miko1034@users.noreply.github.com>
2025-03-04 20:18:33 +0000

    __init__ changes and main changes

--- ebay_notifier/__init__.py
+++ ebay_notifier/__init__.py
@@ -10,22 +10,6 @@ def start():
 			cfg.write("""telegram_bot_token: \ntelegram_channel_id: \n""")
 		print("[+] Config created.")
 
-	# creates the db which will hold the last saved products
-	if "last_saved.db" not in os.listdir():
-		con = sqlite3.connect("last_saved.db")
-		cur = con.cursor()
-		cur.execute("""CREATE TABLE IF NOT EXISTS products(
-					id INTEGER PRIMARY KEY,
-					title text,
-					url text,
-					price text,
-					thumbnail text,
-					shipping_fee,
-					best_offers boolean
-					)""")
-		con.commit()
-		con.close()
-
 	#runs main
 	from ebay_notifier.main import main
 	main()
--- ebay_notifier/main.py
+++ ebay_notifier/main.py
@@ -2,8 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from multiprocessing import Pool, cpu_count
-import sqlite3
 import time
+from urllib.parse import urlparse
+import yaml
 
 def get_random_proxy():
 	pass
@@ -28,12 +29,13 @@ def get_info(url):
 
 
 # should recieve an index from all_li
-def get_details_from_listing(listing):
+def get_listing_details(listing):
 	listing = str(listing)
 	listing_soup = BeautifulSoup(listing, "html.parser")
 
+	listing_url = urlparse(listing_soup.find("a", {"class":"s-item__link"})["href"])._replace(query="").geturl()
 
-	listing_url = listing_soup.find("a", {"class":"s-item__link"})["href"]
+	listing_id = urlparse(listing_url)[2].split("/")[2]
 	listing_price = listing_soup.find("span", {"class":"s-item__price"}).text
 	listing_title = listing_soup.find("div", {"class":"s-item__title"}).text
 
@@ -58,43 +60,85 @@ def get_details_from_listing(listing):
 		"price":listing_price,
 		"thumbnail":listing_thumbnail,
 		"shipping_fee":listing_shipping_fee,
-		"best_offers":listing_best_offers
+		"best_offers":listing_best_offers,
+		"id":listing_id
 	}
 	return product_dict
 
 
-def save_data_to_db(data_dict):
-	con = sqlite3.connect("last_saved.db")
-	cur = con.cursor()
-	cur.execute("INSERT INTO products(title,url,price,thumbnail,shipping_fee,best_offers) VALUES (:title,:url,:price,:thumbnail,:shipping_fee,:best_offers)", data_dict)
-	con.commit()
-	con.close()
+def send_notification(listings):
+	with open("config.yml") as config:
+		try:
+			contents = yaml.safe_load(config)
+			bot_token = contents["telegram_bot_token"]
+			channel_id = contents["telegram_channel_id"]
+		except Exception as e:
+			print(f"[-] Config error: {e}")
+		# title
+		# Current price
+		# If allows offers
+		# Link
+		# Thumbnail
+		for item in listings:
+			message = f"""{item['title']}\n\nPrice: {item['price']}\nOffers enabled: {item['best_offers']}\n{item['thumbnail']}\n\n{item['url']}"""
 
-def check_db_empty():
-	con = sqlite3.connect("last_saved.db")
-	cur = con.cursor()
-	result = cur.execute("SELECT count(*) FROM products").fetchone()[0]
-	
-	if result != 0:
-		return True
-	else:
+			url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={channel_id}&text={message}"
+			resp = requests.get(url)
+
+			if resp.ok:
+				print("[+] New listing. Sent notification.")
+			else:
+				print("[-] Failed to send notification.")
+
+
+
+#essencially l1 - l2
+def find_list_difference(l1,l2):
+	result=[]
+	for i in l1:
+		if i not in l2:
+			result.append(i)
+	return result
+
+
+
+def check_changes(new_data, old_data):
+	sorted_old = sorted(old_data, key=lambda d: d['id'])
+	sorted_new = sorted(new_data, key=lambda d: d['id'])
+	if sorted_old == sorted_new:
 		return False
+	else: # find difference between them as they are not the same
+		old_ids = {item['id'] for item in sorted_old}
+		#new_ids = {item['id'] for item in sorted_new}
 
+		added_items = [item for item in sorted_new if item['id'] not in old_ids]
+		print("\nAdded Items:")
 
-def send_notification():
-	pass
+		new_listings = []
+		for item in added_items:
+			new_listings.append(item)
 
+		return new_listings
 
 def main():
-	raw_product_data = get_info("https://www.ebay.co.uk/sch/i.html?_nkw=nintendo+ds+lite&_sacat=0&LH_BO=1")
+	raw_product_data = [str(i) for i in get_info("https://www.ebay.co.uk/sch/i.html?_nkw=nintendo+ds+lite&_sacat=0&LH_BO=1")]
 	
 	with Pool(cpu_count()) as p:
-		json_product_data = p.map(get_details_from_listing, raw_product_data) # PICKLER ERROR HERE.
+		product_data = p.map(get_listing_details, raw_product_data)
 	
 	print("[+] Finished getting the listings from the initial page.")
-	#product_data = [get_details_from_listing(raw_product_data[3])]
 
-	if not check_db_empty():
-		for pr in product_data:
-			save_data_to_db(pr)
-		print("[+] Initial products saved to DB.")
+	# main loop
+	print("[+] Starting main check loop.")
+	while True:
+		new_raw_product_data = [str(i) for i in get_info("https://www.ebay.co.uk/sch/i.html?_nkw=nintendo+ds+lite&_sacat=0&LH_BO=1")]
+		with Pool(cpu_count()) as p:
+			new_product_data = p.map(get_listing_details, new_raw_product_data)
+
+		new_listings = check_changes(new_product_data, product_data)
+		if new_listings:
+			# if changes detected, this executes
+			send_notification(new_listings)
+
+		product_data = new_listings
+		time.sleep(60) # after the 60 seconds,it spams messages. i dont know why.
