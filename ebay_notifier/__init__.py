e1687f227fc071216282103ce3b18e35cab07a97
mikok <84585119+miko1034@users.noreply.github.com>
2025-03-06 20:40:31 +0000

    added example

--- ebay_notifier/__init__.py
+++ ebay_notifier/__init__.py
@@ -4,7 +4,7 @@ def start():
 	# creates config file
 	if "config.yml" not in os.listdir():
 		with open("config.yml", "w+") as cfg:
-			cfg.write("""telegram_bot_token: \ntelegram_channel_id: \ndelay: # minutes\nwatchlist: [\n{\nurl: "",\nmax_price:  # e.g. £10.00 would be 10.00\n}]""")
+			cfg.write("""telegram_bot_token: \ntelegram_channel_id: \ndelay: # minutes\nwatchlist: [\n{\nurl: "https://example.com",\nmax_price:  # e.g. £10.00 would be 10.00\n}]""")
 		print("[+] Config created.")
 
 	#creates proxy file
