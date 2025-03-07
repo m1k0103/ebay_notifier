f50b1148466a587bec45a917e3984c555f4c8a80
mikok <84585119+miko1034@users.noreply.github.com>
2025-03-07 20:18:12 +0000

    fixed some code that will never run

--- ebay_notifier/main.py
+++ ebay_notifier/main.py
@@ -27,18 +27,15 @@ def main():
 			new_listings = check_changes(new_product_data, product_data[i])
 
 			if new_listings:
-				max_price = watchlist[i]["max_price"]
+				max_price = get_max_price()
 				to_notify_about = []
 				for l in new_listings:
-					if float(l["price"][1:]) <= max_price:
+					if float(l["price"][1:]) <= max_price and l["id"] not in get_already_searched():
 						to_notify_about.append(l)
-						print("[-] A new listing didnt meet the price requirement.")
-				
+						print("[+] A listing is within the price critical region.")
+						add_to_searched(l["id"])
+
 				send_notification(to_notify_about)
-
-				# the stored data is ONLY updated if there are mew listings. this prevents listings 
-				product_data[i] = new_product_data
-
 			else:
 				print("[+] No new listings detected.")
 
@@ -52,40 +49,3 @@ def main():
 
 
 
-	#raw_product_data = [str(i) for i in get_info("https://www.ebay.co.uk/sch/i.html?_nkw=nintendo+ds+lite&LH_BO=1&_ipg=120")]
-	#
-	#with Pool(cpu_count()) as p:
-	#	product_data = p.map(get_listing_details, raw_product_data)
-	#
-	#print("[+] Finished getting the listings from the initial pages.")
-
-	# main loop
-	print("[+] Starting main check loop.")
-	while True:
-		new_raw_product_data = [str(i) for i in get_info("https://www.ebay.co.uk/sch/i.html?_nkw=nintendo+ds+lite&LH_BO=1&_ipg=120")]
-		with Pool(cpu_count()) as p:
-			new_product_data = p.map(get_listing_details, new_raw_product_data)
-
-		new_listings = check_changes(new_product_data, product_data)
-		
-		# if changes detected, this executes
-		if new_listings:
-			max_price = get_max_price()
-			to_notify_about = []
-			for l in new_listings:
-				if float(l["price"][1:]) <= max_price and l["id"] not in get_already_searched():
-					to_notify_about.append(l)
-					print("[+] A listing is within the price critical region.")
-					add_to_searched(l["id"])
-
-			send_notification(to_notify_about)
-		else:
-			print("[+] No new listings detected.")
-
-		# sets the new data to be the current data. the new_listings will hten be 
-		product_data = new_product_data
-		
-		#gets the delay from config, then waits that amount before doing the whole cycle again
-		delay = get_delay()
-		print(f"[!] Waiting {delay} minutes.")
-		time.sleep(60 * delay)
